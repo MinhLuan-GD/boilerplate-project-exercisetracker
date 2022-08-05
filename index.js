3514c84342125b346584ec77c6e6e36b17ba8312
Nguyen Duy Minh Luan <luannguyen11998@gmail.com>
2022-08-05 11:05:06 +0700

    finished

--- index.js
+++ index.js
@@ -24,11 +24,44 @@ app.post("/api/users", async (req, res) => {
 });
 
 app.post("/api/users/:_id/exercises", async (req, res) => {
-  const user = req.params._id;
-  const exercise = await (
-    await exerciseModel.create({ ...req.body, user })
-  ).populate("user");
-  res.json(exercise);
+  const userId = req.params._id;
+  const input = req.body;
+  if (!input.date) input.date = new Date();
+  else input.date = new Date(input.date);
+
+  input.date = input.date.toDateString();
+
+  const exercise = await exerciseModel.create({ ...input, user: userId });
+  const user = await userModel.findById(userId);
+  res.json({
+    _id: user._id,
+    date: exercise.date,
+    duration: exercise.duration,
+    description: exercise.description,
+    username: user.username,
+  });
+});
+
+app.get("/api/users/:_id/logs", async (req, res) => {
+  const user = await userModel.findById(req.params._id);
+  let log = await exerciseModel.find({ user: req.params._id });
+  const count = log.length;
+
+  if (req.query.from && req.query.to)
+    log = log.filter((x) => {
+      const date = new Date(x.date);
+      const from = new Date(req.query.from);
+      const to = new Date(req.query.to);
+      if (date > from && date < to) return true;
+    });
+  if (req.query.limit) log = log.slice(0, req.query.limit);
+
+  res.json({ username: user.username, _id: user._id, count, log });
+});
+
+app.get("/api/users", async (req, res) => {
+  const users = await userModel.find();
+  res.json(users);
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
